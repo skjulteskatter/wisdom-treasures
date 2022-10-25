b8509f4d3d0a57f4ed6ee2ffc556033ec51db663
darianooit <nooit.daria@gmail.com>
2022-10-25 09:30:44 +0200

    adding landing view page

---
+++ src/views/LandingView.vue
@@ -0,0 +1,36 @@
+<template>
+    <main>
+      <h1>Hello, and welcome to the home view</h1>
+    </main>
+  </template>
+  
+  <script lang="ts">
+  import { publicationService } from '@/services/publications';
+  import { defineComponent } from 'vue';
+  
+    export default defineComponent({
+      name: "HomeView",
+      data() {
+        return {
+          publications : [],
+          articles : [],
+        }
+      },
+      props: {
+      },
+      components: {
+      },
+      computed: {
+      },
+      async mounted() {
+        //console.log(await publicationService.list());
+        await publicationService.retrieve({
+          parentIds: ["aa7d92e3-c92f-41f8-87a1-333375125a1c"],
+        }).then(r => {
+          console.log(r);
+        });
+      },
+      methods: {
+      },
+    });
+  </script>
