0bfc9be051cce509d548786f8d646170b1e1e299
TormodKv <tormod@kvitberg.no>
2022-11-04 15:17:37 +0100

    Add profile view for testing

---
+++ src/views/ProfileView.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <h1>Yo {{getFullName}}! Welcome to the profile view. This is mainly created for <b>testing</b> puposes</h1>
+  </main>
+</template>
+
+<script lang="ts">
+import { auth } from '@/services/auth';
+import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: "ProfileView",
+    data() {
+      return {
+        publications : [],
+        articles : [],
+      }
+    },
+    props: {
+    },
+    components: {
+    },
+    computed: {
+      getFullName(): string{
+        return auth.currentUser?.displayName ?? "man";
+      }
+    },
+    methods: {
+    },
+  });
+</script>
