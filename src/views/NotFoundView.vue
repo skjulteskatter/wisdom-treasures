8b9e8129617cc7bfff83f434e3efee5e710e1401
TormodKv <tormod@kvitberg.no>
2022-11-09 17:05:25 +0100

    Better style for 404 page

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,10 +1,11 @@
 <template>
   <main class="col-span-2">
-    <div class="flex items-center flex-col">
-      <div class="text-[6rem] text-primary font">404</div>
-      <div> - Page not Found</div>
-      <ClickableLink v-if="isLoggedIn" @link-clicked="redirect('home')">Go to home page</ClickableLink>
-      <ClickableLink v-else @link-clicked="redirect('login')">Go to login page</ClickableLink>
+    <div class="flex items-center flex-col text-[1.5rem]">
+      <div class="text-[6rem] text-primary font font-bold">404</div>
+      <div class="h-[1px] bg-black/30 w-full" />
+      <div class="mt-3 font-bold">Page not Found</div>
+      <ClickableLink class="font-bold" v-if="isLoggedIn" @link-clicked="redirect('home')">Go to home page</ClickableLink>
+      <ClickableLink class="font-bold" v-else @link-clicked="redirect('login')">Go to login page</ClickableLink>
     </div>
   </main>
 </template>
