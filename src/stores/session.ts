eee18b1a218e7ddb8803df69eca711e5694057ca
TormodKv <tormod@kvitberg.no>
2022-11-16 12:06:41 +0100

    Add search word in store for redirections

--- src/stores/session.ts
+++ src/stores/session.ts
@@ -5,9 +5,12 @@ import i18n from '@/i18n'
 export const useSessionStore = defineStore('session', {
     state: ()=> {
         return {
-            //When store is created take languge from i18n if it exist
+            // When store is created take languge from i18n if it exist
             locale: i18n.global.locale ?? "en",
+            // The name og the page to redirect to if trying to reach restricted to auth site when not logged in
             redirectAfterLoginName: "",
+            // The searchword to automatically search from when pushing to new search view (used in navbar when searching and redirecteing)
+            searchWordBridge: "",
         }
     },
     actions: {
