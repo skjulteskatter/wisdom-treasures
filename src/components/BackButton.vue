29abb85c17e4b6bd5330b54fab6133cd1badf488
TormodKv <tormod@kvitberg.no>
2022-11-18 11:43:35 +0100

    Add back button

---
+++ src/components/BackButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <BaseButton theme="menuButton" @click="navigateBack">
+        <template #default>
+            <div class="text-lg">Back</div>
+        </template>
+        <template #firstIcon>
+            <ArrowLeftIcon class="h-5"/>
+        </template>
+    </BaseButton>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BaseButton from './BaseButton.vue';
+import { ArrowLeftIcon } from '@heroicons/vue/solid';
+import router from '@/router';
+
+export default defineComponent({
+    name: "back-button",
+    props: {
+    },
+    components: {
+        BaseButton,
+        ArrowLeftIcon,
+    },
+    methods: {
+        navigateBack(){
+            if (router.options.history.state.back == null){
+                router.push({name: "dashboard"});
+            } else {
+                router.back();
+            }
+        }
+    }
+});
+</script>
