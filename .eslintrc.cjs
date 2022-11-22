14ad1b601eaddf784157b28c5d31538a90075c81
TormodKv <tormod@kvitberg.no>
2022-11-22 18:11:27 +0100

    Edit readme

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,7 +12,6 @@ module.exports = {
     ecmaVersion: 'latest'
   },
   "rules": {
-    // Note: you must disable the base rule as it can report incorrect errors
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "warn"
   }
