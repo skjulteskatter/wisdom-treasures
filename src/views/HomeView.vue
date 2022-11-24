8126f76ce1e60754041800e77f43700a135b60b5
TormodKv <tormod@kvitberg.no>
2022-11-24 14:29:51 +0100

    Remove loo...ong word

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,7 @@
     </h1>
     <h2>Here's a big list</h2>
     <div v-for="(index) in Array<number>(50)" :key="index">
-      <div>Hello! This is just some random content to try to fill up the page. This is a looooooooooooooooooooooooooooooooooooooooong word</div>
+      <div>Hello! This is just some random content to try to fill up the page. This is a lot of word in a row, just to see how to site scales. Don't worry'bout'it</div>
     </div>
   </main>
 </template>
