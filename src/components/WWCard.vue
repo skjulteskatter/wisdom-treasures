d4e151ba1afdc4ebcd74d0a3217dc2c7d2662abc
TormodKv <tormod@kvitberg.no>
2022-11-28 18:28:34 +0100

    Add clickable wisdomwords

--- src/components/WWCard.vue
+++ src/components/WWCard.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="">
-        <BaseCard class="border-2 hover:border-black/30 border-black/0">
+        <BaseCard class="border-2 hover:border-black/30 border-black/0 cursor-pointer" @click="openWWModal = true">
           <template #default>
             <PencilAltIcon class="w-5"/>
             <p class="serif">
@@ -11,6 +11,9 @@
             {{article.id}}
           </template>
         </BaseCard>
+        <WWCardModal v-if="openWWModal" @close="openWWModal = false" :article="article">
+
+        </WWCardModal>
     </main>
   </template>
     
@@ -19,11 +22,13 @@
   import { defineComponent } from 'vue';
   import BaseCard from './BaseCard.vue';
   import { PencilAltIcon } from '@heroicons/vue/solid';
+  import WWCardModal from './WWCardModal.vue';
   
     export default defineComponent({
       name: "WWCard",
       data() {
         return {
+          openWWModal: false,
         }
       },
       props: {
@@ -35,6 +40,7 @@
       components: {
         BaseCard,
         PencilAltIcon,
+        WWCardModal
       },
       computed: {
         shortContent() {
--- src/components/WWCardModal.vue
+++ src/components/WWCardModal.vue
@@ -1,13 +1,21 @@
 <template>
     <BaseModal :show="true" class="fixed w-full h-full left-0 top-0 z-40" @close="() => $emit('close')">
-        <template #default> 
-            <div class="flex">
-                <p class="grow">
-                    <slot/>
-                </p>
-                <BaseButton theme="menuButton" size="small" class="w-8 self-center max-h-8" @click="() => $emit('close')">
-					<XIcon class="h-8 opacity-50"/>
+        <template #footer>
+            <div class="w-full flex">
+                <div id="justForSpacing" class="grow self-center">See more from this category</div>
+                <BaseButton theme="menuButton" size="small" class="w-8 self-center max-h-8 mx-2" @click="() => {}">
+					<ShareIcon class="h-8 opacity-50"/>
 				</BaseButton>
+                <BaseButton theme="menuButton" size="small" class="w-8 self-center max-h-8 mx-2" @click="() => {}">
+                    <HeartIconSolid class="h-8 heartColor"/>
+					<HeartIcon class="h-8 opacity-50 hidden"/>
+				</BaseButton>
+            </div>
+        </template>
+        <template #default> 
+            <div class="flex max-w-2xl flex-col font-serif">
+                <img src="/img/quote.svg" alt="“" class="self-center max-h-10"/>
+                <div class="grow m-5" v-html="article.content?.content"/>
             </div>
         </template>
     </BaseModal>
@@ -18,28 +26,39 @@
 import BaseModal from "./BaseModal.vue"
 
 import { defineComponent } from "vue";
-import { XIcon } from "@heroicons/vue/solid";
+import { Article } from "hiddentreasures-js";
+import { HeartIcon, ShareIcon } from "@heroicons/vue/outline";
 import BaseButton from "./BaseButton.vue";
+import { HeartIcon as HeartIconSolid } from "@heroicons/vue/solid";
 
 export default defineComponent({
     name: "base-modal",
     components: {
         BaseModal,
-        XIcon,
+        ShareIcon,
         BaseButton,
+        HeartIcon,
+        HeartIconSolid
     },
     data: () => ({
-        searchWord: "" as string,
-        unusedModelValue: "" as string,
     }),
-    emits: ["update:modelValue", "searchAction", "close"],
+    emits: ["close"],
     props: {
-        modelValue: {
-            type: String,
+        article: {
+            type: Article,
+            required: true,
         },
+    },
+    methods: {
+        share(){
+            console.log("Sharing is not implemented yet");
+        }
     },
 });
 </script>
 
 <style>
+.heartColor {
+    filter: invert(22%) sepia(83%) saturate(2467%) hue-rotate(345deg) brightness(100%) contrast(94%);
+}
 </style>
