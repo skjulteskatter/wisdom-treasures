6fcf3b17745ef322ae045744d025440b7b6abebc
TormodKv <tormod@kvitberg.no>
2022-12-08 15:14:27 +0100

    create functions for displaying time since last view in "History"

--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -15,7 +15,7 @@
                       <div class="self-center mr-2">
                         <ClockIcon class="h-8 opacity-50"/>
                       </div>
-                      <div class="self-center opacity-50">Yesterday 13:45</div>
+                      <div class="self-center opacity-50">{{getStringFromDate(historyIds.find(x => x.id == article.id)?.lastViewed ?? new Date)}}</div>
                     </div>
                   </template>
                 </WWCard>
@@ -44,12 +44,13 @@ import { ClockIcon } from '@heroicons/vue/outline';
               store: useSessionStore(),
               articles: [] as Article[],
               periods: {
-                "Today": 0,
-                "This Week": 7,
-                "This Month": 30,
-                "This Year": 365,
-                "Earlier": 2000
-              }
+                "Today": [-1,1],
+                "This Week": [1,7],
+                "This Month": [7,30],
+                "This Year": [30,365],
+                "Earlier": [365,1000],
+              } as {[key:string]: [number, number]},
+              historyIds: [] as {id: string, lastViewed: Date}[]
           }
       },
       props: {
@@ -66,10 +67,49 @@ import { ClockIcon } from '@heroicons/vue/outline';
       },
       mounted() {
         this.articles = TestArticles;
+        this.setHistoryIds();
       },
       methods: {
-        getArticlesFromDays(days: number) : Article[]{
-          return this.articles.slice(days,days+2);
+        getArticlesFromDays(days: [number, number]) : Article[]{
+          let IDs : string[] = this.historyIds.filter(x => {
+
+            //Create date without hours, minutes...
+            let y = new Date(x.lastViewed.getTime());
+            y.setHours(0,0,0,0);
+
+            //Create date without hours, minutes...
+            let dateNow = new Date();
+            dateNow.setHours(0,0,0,0);
+
+            return y.getTime() <= (dateNow.getTime() - (days[0] * 24 * 60 * 60 * 1000)) && y.getTime() > (dateNow.getTime() - (days[1] * 24 * 60 * 60 * 1000))
+          }).map(x => x.id);
+          return this.articles.filter(x => IDs.includes(x.id)).sort((a,b) => {
+            if ((this.historyIds.find(x => x.id == a.id)?.lastViewed ?? new Date()) < (this.historyIds.find(x => x.id == b.id)?.lastViewed ?? new Date())) return 1;
+            return -1;
+          })
+        },
+        setHistoryIds(){
+          for (const key in localStorage) {
+            if (key.startsWith('history:')) {
+              this.historyIds.push({id: key.replace('history:', ''), lastViewed: new Date(+(localStorage.getItem(key) ?? Date.now()))});
+            }
+          }
+        },
+        getStringFromDate(date: Date): string {
+          let days : string[] = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+          let timeDiff = Date.now() - date.getTime();
+
+          if (timeDiff < 60 * 1000){
+            return "Just now";
+          } else if (timeDiff < 60 * 60 * 1000){
+            return `${(timeDiff/1000/60).toFixed()} miuntes ago`;
+          } else if (timeDiff < 24 * 60 * 60 * 1000){
+            return `${(timeDiff/1000/60/60).toFixed()} hours ago`;
+          } else if (Date.now() - date.getTime() < 24 * 6){
+            return days[date.getDay()];
+          } else 
+          return `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`;
         }
       }
     });
