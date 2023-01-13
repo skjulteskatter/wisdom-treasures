414e6273a54f5726332c3421eca29bed18a68c9c
darianooit <nooit.daria@gmail.com>
2023-01-13 11:45:29 +0100

    ContactView

---
+++ src/views/ContactView.vue
@@ -0,0 +1,18 @@
+<template>
+    <main>
+        <h1>that is contact view</h1>
+    </main>
+</template>
+  
+  <script lang="ts">
+  import { defineComponent } from 'vue';
+  
+    export default defineComponent({
+      name: "ContactView",
+      components: {  }
+    })
+
+  </script>
+  
+  <style>
+  </style>
