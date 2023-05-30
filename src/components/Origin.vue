d3392d2fd8ccb7aeb4a0a688b8284686630a04c9
darianooit <nooit.daria@gmail.com>
2023-05-30 11:02:59 +0200

    more space, wwshowcard down on hp

--- src/components/Origin.vue
+++ src/components/Origin.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex flex-col justify-center items-center">
+    <div class="flex flex-col justify-between items-center">
         <div class="w-24 h-24 flex justify-center items-center bg-[color:var(--wt-c-white-mute)] rounded-xl">
             <h1 class="text-2xl font-bold text-primary">SS</h1>
         </div>
-        <h2>Origins name</h2>
+        <h2 class="mb-5">Origins name</h2>
     </div>
 </template>
 
