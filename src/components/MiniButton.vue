1ca656d5362e1e9e3e30a459c7efe880bca75500
krisssybq <kristianebratlie@gmail.com>
2023-06-22 17:00:36 +1200

    Add untracket files

---
+++ src/components/MiniButton.vue
@@ -0,0 +1,44 @@
+<template>
+    <main>
+        <BaseButton @click="clicked" :threeD="true" :loading="loading" :loadingLabel="loadingLabel" theme="noBackground">
+            <template #default>
+                <slot/>
+            </template>
+            <template v-if="$slots.icon" #firstIcon>
+                <slot name="firstIcon"/>
+            </template>
+            <template v-if="$slots.icon" #icon>
+                <slot name="icon"/>
+            </template>
+        </BaseButton>
+        <!-- <div id="buttonBackground" class="overflow-hidden bg-primary brightness-50 w-[calc(100%-2px)] h-[calc(100%-4px)] ml-[1px] mt-1 absolute rounded-md top-0"/> -->
+    </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BaseButton from './BaseButton.vue';
+
+export default defineComponent({
+    name: "Three-d-button",
+    components: {
+        BaseButton
+    },
+    emits: ["clicked"],
+    methods: {
+        clicked(){
+            this.$emit('clicked');
+        }
+    },
+    props: {
+        loading: {
+            type: Boolean,
+            default: false,
+        },
+        loadingLabel: {
+            type: String,
+            default: "Loading...",
+        }
+    },
+});
+</script>
