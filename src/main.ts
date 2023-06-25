17fe0fde90b99e4e7b86e6f5b7f1fe13f75b91e7
darianooit <nooit.daria@gmail.com>
2023-06-25 22:41:25 +0200

    Font problem commented!!?? // import '@fortawesome/fontawesome-free/css/all.css';

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ import App from './App.vue';
 import router from './router';
 
 import './assets/main.css';
-import '@fortawesome/fontawesome-free/css/all.css';
+// import '@fortawesome/fontawesome-free/css/all.css';
 
 const app = createApp(App);
 
