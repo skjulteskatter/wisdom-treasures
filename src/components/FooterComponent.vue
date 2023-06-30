4e09b8f09a70f0d44e3160247a0854932a499a50
darianooit <nooit.daria@gmail.com>
2023-06-30 11:19:33 +0200

    Footer smaller and lighter

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <p class="text-xs sm:text-sm text-[color:var(--wt-color-text-grey)] opacity-50 text-center py-4 px-5" >
-      Copyright 2022 © Stiftelsen Skjulte Skatters Forlag. All Rights Reserved
+    <p class="text-xxs text-[color:var(--wt-color-text-grey)] opacity-30 text-center py-4 px-5" >
+      © 2022 Stiftelsen Skjulte Skatters Forlag. All Rights Reserved
     </p>
   </div>
 </template>
@@ -13,3 +13,8 @@ export default defineComponent({
     name: "FooterComponent",
 });
 </script>
+<style>
+.text-xxs{
+  font-size: 0.65rem;
+}
+</style>
