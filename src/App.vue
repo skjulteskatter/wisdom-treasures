bde1fb64889db1c45da8809b5ecaa38b47eddcce
krisssybq <kristianebratlie@gmail.com>
2023-08-23 12:52:09 +0200

    Removeed suggestion form from login view

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
         <RouterView />
-        <SuggestionForm/>
 </template>
 
 <script setup lang="ts">
 import { onBeforeMount } from 'vue';
 import { RouterView } from 'vue-router'
 import { auth, getCurrentUserPromise } from './services/auth';
-import SuggestionForm from './components/SuggestWWForm.vue'
 onBeforeMount(async () => {
     // Get the user from firebase if it exist. Just to make sure redirects work properly
     if (auth.currentUser) return;
